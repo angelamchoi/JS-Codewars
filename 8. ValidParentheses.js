56b39dc8968c9809bd2f2aa39b364c6f85aaa0bc
Angela Choi <angelamchoi@gmail.com>
2023-01-25 18:37:43 -0500

    added valid parentheses

---
+++ 8. ValidParentheses.js
@@ -0,0 +1,19 @@
+// Instructions:
+// Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
+
+// Examples
+// "()"              =>  true
+// ")(()))"          =>  false
+// "("               =>  false
+// "(())((()())())"  =>  true
+
+
+function validParentheses(parens) {
+  let result = 0;
+
+  for (var i = 0 ; i < parens.length && result>= 0; i++) {
+    result+= (parens[i] == '(') ? 1 : -1;    
+  }
+
+  return (result == 0);
+}
